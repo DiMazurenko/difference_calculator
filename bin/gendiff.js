25658d765145183c6f51faf538652715b8ced205
Dmitriy <dimazurenko@gmail.com>
2022-07-18 17:35:45 +0300

    feat add arguments and option -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-import { Command } from '../node_modules/commander/esm.mjs';
-const program = new Command();
+import { program } from 'commander';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse(process.argv);
