28426cf287078fe0aa5a729eb5d01bcb618df35d
Dmitriy <dimazurenko@gmail.com>
2022-07-24 17:02:12 +0300

    feat get file format

--- src/utility.js
+++ src/utility.js
@@ -8,3 +8,5 @@ export const getPath = (path) => resolve(cwd(), path);
 export const readFile = (path) => readFileSync(getPath(path), 'utf-8');
 
 export const sortUnionArrays = (arr1, arr2) => _.sortBy(_.union(arr1, arr2));
+
+export const getFileName = (path) => path.split('.').pop();
