d44a568768c6e1d0698e98f06a744b5143d66689
Dmitriy <dimazurenko@gmail.com>
2022-07-24 17:02:36 +0300

    test gendiff .yaml

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -4,11 +4,20 @@ import { dirname } from 'path';
 import genDiff from '../src/index.js';
 import { readFile } from '../src/utility.js';
 
-test('genDiff', () => {
+test('genDiff json', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.json`;
   const path2 = `${__dirname}/../__fixtures__/file2.json`;
+  const result = readFile(`${__dirname}/../__fixtures__/result_file1_file2`);
+  expect(genDiff(path1, path2)).toEqual(result);
+});
+
+test('genDiff yaml', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
+  const path2 = `${__dirname}/../__fixtures__/file2.yml`;
   const result = readFile(`${__dirname}/../__fixtures__/result_file1_file2`);
   expect(genDiff(path1, path2)).toEqual(result);
 });
