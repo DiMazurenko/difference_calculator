3823719671050f0a0b3e9e76756b2348a5639e6e
Dmitriy <dimazurenko@gmail.com>
2022-07-25 17:58:51 +0300

    test added tests format json

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -39,3 +39,21 @@ test('genDiff yaml plain', () => {
   const result = readFile(`${__dirname}/../__fixtures__/result_Plain`);
   expect(genDiff(path1, path2, 'plain')).toEqual(result);
 });
+
+test('genDiff json json', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.json`;
+  const path2 = `${__dirname}/../__fixtures__/file2.json`;
+  const result = readFile(`${__dirname}/../__fixtures__/result_JSON`);
+  expect(genDiff(path1, path2, 'json')).toEqual(result);
+});
+
+test('genDiff yaml json', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
+  const path2 = `${__dirname}/../__fixtures__/file2.yml`;
+  const result = readFile(`${__dirname}/../__fixtures__/result_JSON`);
+  expect(genDiff(path1, path2, 'json')).toEqual(result);
+});
