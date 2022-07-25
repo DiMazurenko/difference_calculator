ce929d341b2b3855cdba98e6c0c6d2488d4483cb
Dmitriy <dimazurenko@gmail.com>
2022-07-25 17:33:22 +0300

    test added test format plain

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -4,20 +4,38 @@ import { dirname } from 'path';
 import genDiff from '../src/index.js';
 import { readFile } from '../src/utility.js';
 
-test('genDiff json', () => {
+test('genDiff json stylish', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.json`;
   const path2 = `${__dirname}/../__fixtures__/file2.json`;
   const result = readFile(`${__dirname}/../__fixtures__/result_Stylish`);
-  expect(genDiff(path1, path2)).toEqual(result);
+  expect(genDiff(path1, path2, 'stylish')).toEqual(result);
 });
 
-test('genDiff yaml', () => {
+test('genDiff yaml stylish', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
   const path2 = `${__dirname}/../__fixtures__/file2.yml`;
   const result = readFile(`${__dirname}/../__fixtures__/result_Stylish`);
-  expect(genDiff(path1, path2)).toEqual(result);
+  expect(genDiff(path1, path2, 'stylish')).toEqual(result);
+});
+
+test('genDiff json plain', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.json`;
+  const path2 = `${__dirname}/../__fixtures__/file2.json`;
+  const result = readFile(`${__dirname}/../__fixtures__/result_Plain`);
+  expect(genDiff(path1, path2, 'plain')).toEqual(result);
+});
+
+test('genDiff yaml plain', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
+  const path2 = `${__dirname}/../__fixtures__/file2.yml`;
+  const result = readFile(`${__dirname}/../__fixtures__/result_Plain`);
+  expect(genDiff(path1, path2, 'plain')).toEqual(result);
 });
