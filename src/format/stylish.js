d8d6b8e005f0616155ec0a8e03a79583ade2e2ab
Dmitriy <dimazurenko@gmail.com>
2022-07-25 15:18:46 +0300

    feat stylish

---
+++ src/format/stylish.js
@@ -0,0 +1,67 @@
+import _ from 'lodash';
+import { makeIndent } from '../utility.js';
+
+export default (tree) => {
+  const newValue = (value, depth = 1) => {
+    if (!_.isPlainObject(value)) {
+      return `${value}`;
+    }
+
+    const keys = Object.keys(value);
+    const result = keys.map((key) => {
+      const pare = value[key];
+      return `${makeIndent(depth + 1)}  ${key}: ${newValue(pare, depth + 1)}`;
+    });
+
+    return `{\n${result.join('\n')}\n  ${makeIndent(depth)}}`;
+  };
+
+  const iter = (node, depth = 0) => {
+    const {
+      key, value, type, children, value1, value2
+    } = node;
+
+    switch (type) {
+      case 'root': {
+        const newChildren = children.flatMap((child) => iter(child, depth + 1));
+        return `{\n${newChildren.join('\n')}\n}`;
+      }
+
+      case 'parent': {
+        const newChildren = children.flatMap((child) => iter(child, depth + 1));
+        return `${makeIndent(depth)}  ${key}: {\n${newChildren.join(
+          '\n'
+        )}\n${makeIndent(depth)}  }`;
+      }
+
+      case 'added': {
+        return `${makeIndent(depth)}+ ${key}: ${newValue(value, depth)}`;
+      }
+
+      case 'deleted': {
+        return `${makeIndent(depth)}- ${key}: ${newValue(value, depth)}`;
+      }
+
+      case 'changed': {
+        const firstElement = `${makeIndent(depth)}- ${key}: ${newValue(
+          value1,
+          depth
+        )}`;
+        const secondElement = `${makeIndent(depth)}+ ${key}: ${newValue(
+          value2,
+          depth
+        )}`;
+        return `${firstElement}\n${secondElement}`;
+      }
+
+      case 'unchanged': {
+        return `${makeIndent(depth)}  ${key}: ${newValue(value1, depth)}`;
+      }
+
+      default:
+        throw new Error(`Unknown type: '${type}' of node!`);
+    }
+  };
+
+  return iter(tree);
+};
