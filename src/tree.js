bfd0e42c8e937faa7b462eb6927b89f224903fad
Dmitriy <dimazurenko@gmail.com>
2022-07-25 20:38:09 +0300

    style update tree.js

--- src/tree.js
+++ src/tree.js
@@ -2,53 +2,31 @@ import _ from 'lodash';
 import { sortUnionArrays } from './utility.js';
 
 const tree = (objOne, objTwo) => {
-  const keysObjOne = Object.keys(objOne);
-  const keysObjTwo = Object.keys(objTwo);
-
-  const sortKeys = sortUnionArrays(keysObjOne, keysObjTwo);
+  const sortKeys = sortUnionArrays(Object.keys(objOne), Object.keys(objTwo));
 
   const result = sortKeys.map((key) => {
     const value1 = objOne[key];
     const value2 = objTwo[key];
 
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return {
-        key,
-        type: 'parent',
-        children: tree(value1, value2),
-      };
+      return { key, type: 'parent', children: tree(value1, value2), };
     }
 
     if (!_.has(objOne, key)) {
-      return {
-        key,
-        type: 'added',
-        value: value2,
-      };
+      return { key, type: 'added', value: value2, };
     }
 
     if (!_.has(objTwo, key)) {
-      return {
-        key,
-        type: 'deleted',
-        value: value1,
-      };
+      return { key, type: 'deleted', value: value1, };
     }
 
     if (value1 !== value2) {
       return {
-        key,
-        type: 'changed',
-        value1,
-        value2,
+        key, type: 'changed', value1, value2,
       };
     }
 
-    return {
-      key,
-      type: 'unchanged',
-      value: value1,
-    };
+    return { key, type: 'unchanged', value: value1, };
   });
 
   return result;
