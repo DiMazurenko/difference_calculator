852ed99067b1f5a21c2251445dfb2c77ab874b33
Dmitriy <dimazurenko@gmail.com>
2022-07-25 19:11:25 +0300

    test added test borderline cases

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -4,7 +4,7 @@ import { dirname } from 'path';
 import genDiff from '../src/index.js';
 import { readFile } from '../src/utility.js';
 
-test('genDiff json stylish', () => {
+test('genDiff file:json formst:stylish', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.json`;
@@ -13,7 +13,7 @@ test('genDiff json stylish', () => {
   expect(genDiff(path1, path2, 'stylish')).toEqual(result);
 });
 
-test('genDiff yaml stylish', () => {
+test('genDiff file:yaml format:stylish', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
@@ -22,7 +22,7 @@ test('genDiff yaml stylish', () => {
   expect(genDiff(path1, path2, 'stylish')).toEqual(result);
 });
 
-test('genDiff json plain', () => {
+test('genDiff file:json format:plain', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.json`;
@@ -31,7 +31,7 @@ test('genDiff json plain', () => {
   expect(genDiff(path1, path2, 'plain')).toEqual(result);
 });
 
-test('genDiff yaml plain', () => {
+test('genDiff file:yaml format:plain', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
@@ -40,7 +40,7 @@ test('genDiff yaml plain', () => {
   expect(genDiff(path1, path2, 'plain')).toEqual(result);
 });
 
-test('genDiff json json', () => {
+test('genDiff file:json format:json', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.json`;
@@ -49,11 +49,42 @@ test('genDiff json json', () => {
   expect(genDiff(path1, path2, 'json')).toEqual(result);
 });
 
-test('genDiff yaml json', () => {
+test('genDiff file:yaml format:json', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const path1 = `${__dirname}/../__fixtures__/file1.yaml`;
   const path2 = `${__dirname}/../__fixtures__/file2.yml`;
   const result = readFile(`${__dirname}/../__fixtures__/result_JSON`);
   expect(genDiff(path1, path2, 'json')).toEqual(result);
+});
+
+test('genDiff wrong result file:json format:stylish', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.json`;
+  const path2 = `${__dirname}/../__fixtures__/file2.json`;
+  const result = readFile(`${__dirname}/../__fixtures__/wrong_result_Stylish`);
+  expect(genDiff(path1, path2)).not.toEqual(result);
+});
+
+test('genDiff wrong filename file:json format:stylish', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.json`;
+  const path2 = `${__dirname}/../__fixtures__/file.txt`;
+  const filename = 'txt';
+  expect(() => genDiff(path1, path2)).toThrow(
+    `Unknown filename: '${filename}'!`
+  );
+});
+
+test('genDiff wrong format file:json format:stylish', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path1 = `${__dirname}/../__fixtures__/file1.json`;
+  const path2 = `${__dirname}/../__fixtures__/file2.json`;
+  const formatName = 'yaml';
+  expect(() => genDiff(path1, path2, formatName)).toThrow(
+    `Unknown format: '${formatName}'!`
+  );
 });
